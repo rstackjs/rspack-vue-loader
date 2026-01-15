c1fc1ebb616a4191512330cd3212ad1e314846d3
9aoy <9aoyuao@gmail.com>
2026-01-15 15:00:03 +0800

    fix: use node 20

--- rstest.config.ts
+++ rstest.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from '@rstest/core';
 
-const __dirname = new URL('.', import.meta.url).pathname;
-
 export default defineConfig({
-  root: __dirname,
   globals: true,
   output: {
     externals: {
-      'rspack-vue-loader': `commonjs ${__dirname}/dist/index.js`,
+      'rspack-vue-loader': `commonjs ../dist/index.js`,
     },
   },
-  disableConsoleIntercept: true,
 });
