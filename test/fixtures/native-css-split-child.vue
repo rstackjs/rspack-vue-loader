966a624656b9d265afd5c446d177b75509e8771b
intellild <intellild@outlook.com>
2026-07-16 08:33:53 +0000

    test: cover native CSS build modes

---
+++ test/fixtures/native-css-split-child.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="native-child">native styled child</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.native-child {
+  color: tomato;
+}
+</style>
---
+++ test/fixtures/native-css-ssr-entry.js
@@ -0,0 +1,3 @@
+import Component from './native-css-modules.vue'
+
+export default Component
