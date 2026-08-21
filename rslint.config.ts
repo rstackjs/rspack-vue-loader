5ac0d9f5366ca22869dfde71f0a934a7dacdaa72
neverland <chenjiahan.jait@bytedance.com>
2026-08-21 14:27:28 +0800

    chore: disable no-undef globally

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, globals, js, ts } from '@rslint/core';
+import { defineConfig, js, ts } from '@rslint/core';
 
 export default defineConfig([
   js.configs.recommended,
@@ -9,43 +9,7 @@ export default defineConfig([
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-this-alias': 'off',
-    },
-  },
-  {
-    files: ['example/{devServer,ssr,webpack.config}.js'],
-    languageOptions: {
-      globals: globals.node,
-    },
-  },
-  {
-    files: ['example/main.js'],
-    languageOptions: {
-      globals: {
-        __IS_SSR__: 'readonly',
-      },
-    },
-  },
-  {
-    files: ['**/*.test.{ts,tsx}'],
-    languageOptions: {
-      globals: globals.rstest,
-    },
-  },
-  {
-    files: ['test/fixtures/**/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.rstest,
-      },
-    },
-  },
-  {
-    files: ['src/**/*.js'],
-    languageOptions: {
-      globals: {
-        require: 'readonly',
-      },
+      'no-undef': 'off',
     },
   },
 ]);
